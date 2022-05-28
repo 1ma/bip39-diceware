5611d59e8c333d59141b0773a8557ccb8415b684
Marcel Hernandez <e6990620@gmail.com>
2022-05-28 20:04:12 +0200

    checksum.sh: add guardrails

--- checksum.sh
+++ checksum.sh
@@ -3,15 +3,35 @@
 # https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
 set -euo pipefail
 
-echo -n "Enter desired mnemonic length (12|15|18|21|24): "
+echo
+echo "WARNING: Don't run this script on an internet-enabled device unless you are just trying it out with fake data"
+echo
+
+echo "Enter desired number of words for your mnemonic seed."
+echo -n "The BIP-39 spec allows 12, 15, 18, 21 or 24: "
 read -r MNEMONIC_LENGTH
 
+if [[ ! $MNEMONIC_LENGTH =~ ^[0-9]{2}$ || ($MNEMONIC_LENGTH -ne 12 && $MNEMONIC_LENGTH -ne 15 && $MNEMONIC_LENGTH -ne 18 && $MNEMONIC_LENGTH -ne 21 && $MNEMONIC_LENGTH -ne 24) ]];
+then
+  echo "Invalid input, GFY"
+  exit 1
+fi
+
+echo
+echo "Select $((MNEMONIC_LENGTH-1)) random words from the diceware lists and type in each associated number. Select the words using the procedure described in the document."
 ENT_BITS=""
 COUNTER=1
 while [ "$COUNTER" -lt "$MNEMONIC_LENGTH" ]
 do
   echo -n "Enter number $COUNTER/$((MNEMONIC_LENGTH-1)): "
   read -r NUMBER
+
+  if [[ ! $NUMBER =~ ^[0-9]+$ || $NUMBER -lt 0 || $NUMBER -gt 2047 ]];
+  then
+    echo "Wrong input. This is really not a good time to fuck around, WAKE UP MF."
+    exit 1
+  fi
+
   NUMBER_BITS=$(printf "%011d" "$(echo "obase=2;${NUMBER}" | bc)")
   ENT_BITS="$ENT_BITS$NUMBER_BITS"
   COUNTER=$((COUNTER+1))
@@ -33,6 +53,13 @@ while [ "$COUNTER" -le "$REMAINDER" ]
 do
   echo -n "Enter bit $COUNTER/$REMAINDER: "
   read -r BIT
+
+  if [[ ! $BIT =~ ^[0-1]{1}$ ]];
+  then
+    echo "Wrong input. This is really not a good time to fuck around, WAKE UP MF."
+    exit 1
+  fi
+
   FINAL_ENT_BITS="$FINAL_ENT_BITS$BIT"
   COUNTER=$((COUNTER+1))
 done
